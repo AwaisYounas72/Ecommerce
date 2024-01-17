91364aa58f2deae81127b8799515d37d6964f832
AwaisYounas72 <awais_younas_intern@netixsol.com>
2024-01-17 14:32:31 +0500

    hh

--- ecommerce-admin-master/pages/api/auth/[...nextauth].js
+++ ecommerce-admin-master/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['muhammadawaisyounas72@gmail.com','muhammadabubakar7526@gmail.com','pub911286gmail.com','gorayausman061@gmail.com'];
+// const adminEmails = ['muhammadawaisyounas72@gmail.com','muhammadabubakar7526@gmail.com','pub911286gmail.com','gorayausman061@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -16,11 +16,11 @@ export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: ({session,token,user}) => {
-      if (adminEmails.includes(session?.user?.email)) {
+      // if (adminEmails.includes(session?.user?.email)) {
         return session;
-      } else {
-        return false;
-      }
+      // } else {
+      //   return false;
+      // }
     },
   },
 };
@@ -29,9 +29,9 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req,res) {
   const session = await getServerSession(req,res,authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end();
-    throw 'not an admin';
-  }
+  // if (!adminEmails.includes(session?.user?.email)) {
+  //   res.status(401);
+  //   res.end();
+  //   throw 'not an admin';
+  // }
 }
