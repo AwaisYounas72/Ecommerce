1f7f03422b65edc61d75d528de7e73f167b7e579
AwaisYounas72 <awais_younas_intern@netixsol.com>
2024-01-17 13:13:06 +0500

    emailadded

--- ecommerce-admin-master/pages/api/auth/[...nextauth].js
+++ ecommerce-admin-master/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['muhammadawaisyounas72@gmail.com','muhammadabubakar7526@gmail.com'];
+const adminEmails = ['muhammadawaisyounas72@gmail.com','muhammadabubakar7526@gmail.com','pub911286gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
